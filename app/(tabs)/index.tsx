d99c5b5dcb4f8b3f2f8c7ea2ca7b11cb9bfa6046
Amandeep Singh <programmeramandeep@gmail.com>
2025-04-11 15:52:26 +0530

    feat: Chapter 6 - implement gesture handling for emoji stickers; add double-tap scaling and drag functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import { type ImageSource } from "expo-image";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -50,7 +51,7 @@ export default function Index() {
 	};
 
 	return (
-		<View style={styles.container}>
+		<GestureHandlerRootView style={styles.container}>
 			<View style={styles.imageContainer}>
 				<ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
 				{pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -72,7 +73,7 @@ export default function Index() {
 			<EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
 				<EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
 			</EmojiPicker>
-		</View>
+		</GestureHandlerRootView>
 	);
 }
 
